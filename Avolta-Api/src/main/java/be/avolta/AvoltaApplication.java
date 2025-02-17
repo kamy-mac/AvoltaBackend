c25c13291951c4c72fc06268fc70ed30443a221b
kamy-mac <kama2109@student.eafc-uccle.be>
2025-02-17 18:58:14 +0100

    Commit 1 : Initialisation du projet Api

---
+++ Avolta-Api/src/main/java/be/avolta/AvoltaApplication.java
@@ -0,0 +1,13 @@
+package be.avolta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvoltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvoltaApplication.class, args);
+	}
+
+}
